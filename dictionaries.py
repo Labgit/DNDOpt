d0c77e7a1e8b0c0d561966762a5a2e13f080ce26
Labgit <nickhutcherson@gmail.com>
2016-05-03 07:42:08 -0600

    combined testUI and UI. Everything is functioning correctly. Yay!

---
+++ dictionaries.py
@@ -0,0 +1,24 @@
+abilities_dict = {
+    'Barbarian': {
+        '1':['Rage', 'Unarmored Defense'],
+        '2':['Reckless Attack', 'Danger Sense'],
+        '3':['Primal Path'],
+        '5':['Extra Attack', 'Fast Movement'],
+        '7':['Feral Instict'],
+        '9':['Brutal Critical'],
+        '11':['Relentless Rage'],
+        '15':['Persistent Rage'],
+        '18':['Indomitable Might'],
+        '20':['Primal Champion']
+    },
+    'Bard': {
+        '1':['Bardic Inspiration'],
+        '2':['Jack of All Trades', 'Song of Rest'],
+        '3':['Expertise'],
+        '5':['Font of Inspiration'],
+        '6':['Countercharm'],
+        '10':['Magical Secrets'],
+        '20':['Superior Inspiration']
+    }
+
+}
