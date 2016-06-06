57af6a93ee1ce0f41e86e931b920a99e147da6da
Labgit <nickhutcherson@gmail.com>
2016-06-05 18:54:16 -0600

    combined testUI and UI. Everything is functioning correctly. Yay!

--- dictionaries.py
+++ dictionaries.py
@@ -1,15 +1,33 @@
 abilities_dict = {
     'Barbarian': {
-        '1':['Rage', 'Unarmored Defense'],
-        '2':['Reckless Attack', 'Danger Sense'],
-        '3':['Primal Path'],
-        '5':['Extra Attack', 'Fast Movement'],
-        '7':['Feral Instict'],
-        '9':['Brutal Critical'],
-        '11':['Relentless Rage'],
-        '15':['Persistent Rage'],
-        '18':['Indomitable Might'],
-        '20':['Primal Champion']
+        'Standard Abilities' : {
+            '1':['Rage', 'Unarmored Defense'],
+            '2':['Reckless Attack', 'Danger Sense'],
+            '3':['Primal Path'],
+            '5':['Extra Attack', 'Fast Movement'],
+            '7':['Feral Instict'],
+            '9':['Brutal Critical'],
+            '11':['Relentless Rage'],
+            '15':['Persistent Rage'],
+            '18':['Indomitable Might'],
+            '20':['Primal Champion']
+        },
+        'Primal Paths' : {
+            'Path of the Berserker': {
+                '3':['Frenzy'],
+                '6':['Mindless Rage'],
+                '10':['Intimidating Presence']
+                '14':['Retaliation']
+            },
+            'Path of the Totem Warrior': {
+                '3':['Spirit Seeker', 'Totem Spirit'],
+                '6':['Aspect of the Beast'],
+                '10':['Spirit Walker'],
+                '14':['Totemic Attunement'],
+                
+            }
+        }
+
     },
     'Bard': {
         '1':['Bardic Inspiration'],
